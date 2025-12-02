08b2b87e26cb6233c7f9a543da2a4baa0cdc763f
Michael C. Daconta <michael.daconta@gmail.com>
2025-12-01 21:15:20 -0500

    Added logging

--- src/main/java/us/daconta/xlmeco/provider/GenerativeProviderFactory.java
+++ src/main/java/us/daconta/xlmeco/provider/GenerativeProviderFactory.java
@@ -1,12 +1,16 @@
 package us.daconta.xlmeco.provider;
 
+import us.daconta.xlmeco.GrpcXlmServer;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.ServiceLoader;
+import java.util.logging.Logger;
 
 public class GenerativeProviderFactory {
 
+    private static final Logger logger = Logger.getLogger(GenerativeProviderFactory.class.getName());
     private static ServiceLoader<GenerativeProvider> loader = ServiceLoader.load(GenerativeProvider.class);
 
     public static GenerativeProvider getProvider(String providerName) {
@@ -23,11 +27,14 @@ public class GenerativeProviderFactory {
 
         for (GenerativeProvider provider : loader) {
             String providerName = provider.getProviderName().toLowerCase();
+            logger.info("Loading provider: " + providerName);
             Properties providerProps = filterPropertiesForPrefix(properties, providerName + ".");
 
             if (!providerProps.isEmpty()) {
                 provider.initialize(providerProps);  // Initialize with filtered properties
                 providers.put(providerName, provider);
+            } else {
+                logger.info("Provider properties are empty for: " + providerName);
             }
         }
         return providers;
